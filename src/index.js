33f8d43fb736cf69d9299a53c1815e5f0390b7b0
Javier <51manobandajavier@gmail.com>
2021-04-14 08:22:45 -0500

    :heavy_plus_sign: webpack production
    add scripts to production

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles/global.scss";
+
+//estamos llamando a la aplicación
+ReactDOM.render(<App />, document.getElementById("app"));
